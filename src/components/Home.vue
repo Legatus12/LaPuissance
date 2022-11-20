bc2f6bb7d98c3e5eefc3fc134105b83a7dd7be81
Legatus12 <pablo.clase.daw@gmail.com>
2022-11-20 13:39:22 +0100

    20/11/22

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,36 +1,38 @@
 <template>
-    <div class="w-full h-full bg-[#f6f6f6] flex flex-col lg:flex-row md:flex-row gap-0.5 overflow-hidden">
+    <div class="w-full h-full bg-[#f6f6f6] flex flex-col lg:flex-row md:flex-row gap-0 overflow-hidden">
         
-        <div class="basis-1/2 w-full h-1/2 md:h-full bg-showcase bg-cover bg-center flex p-8">
-            <div class="bg-[#232323] flex h-fit p-4 gap-4 hidden lg:flex">
-                <img src="../assets/img/escudo.png" class="w-16 h-20">
+        <div class="basis-1/2 w-full h-1/2 md:h-full bg-showcase bg-cover bg-center flex flex-col justify-start p-8">
+            <!--
+            <div class="flex w-fit h-fit gap-4 lg:flex bg-[#f6f6f6] border-solid border-2 border-[#232323] hidden md:flex">
+                <img src="../assets/img/escudonn.png" class="w-16 p-1">
                 <div class="w-fit p-2">
-                    <h1 class="text-3xl font-bold text-[#f6f6f6]">La&nbsp;Puissance&nbsp;</h1>
-                    <p class="text-[#f1121f]">Asociación deportiva.</p>
+                    <p class="text-[#232323] w-fit">Asociación deportiva</p>
+                    <h1 class="text-3xl lg:text-4xl font-bold text-[#232323]">La&nbsp;Puissance&nbsp;</h1>
                 </div>
             </div>
+            -->
         </div>
         
-        <div class="bg-[#f6f6f6] cursor-pointer basis-1/2 w-full h-1/2 md:h-full flex flex-col xl:grid grid-cols-2 grid-rows-2 gap-0.5">
-            <div class="bg-g4 bg-cover bg-center p-8 h-1/4 xl:h-full flex flex-col justify-center md:justify-between"
+        <div class="bg-[#f6f6f6] cursor-pointer basis-1/2 w-full h-1/2 md:h-full flex flex-col xl:grid grid-cols-2 grid-rows-2 gap-0">
+            <div class="bg-g4 bg-cover bg-center hover:bg-left duration-300 p-8 h-1/4 xl:h-full flex flex-col justify-center md:justify-between"
             @click="render('Season')">
                 <h1 class="text-5xl font-bold">Temporada</h1>
                 <p class="text-xl hidden md:inline"></p>
             </div>
 
-            <div class="bg-g2 bg-cover bg-center p-8 h-1/4 xl:h-full flex flex-col justify-center md:justify-between"
+            <div class="bg-g2 bg-cover bg-center hover:bg-left duration-300 p-8 h-1/4 xl:h-full flex flex-col justify-center md:justify-between"
             @click="render('Shop')">
                 <h1 class="text-5xl font-bold">Tienda</h1>
                 <p class="text-xl hidden md:inline"></p>
             </div>
 
-            <div class="bg-g1 bg-cover bg-center p-8 h-1/4 xl:h-full flex flex-col justify-center md:justify-between"
+            <div class="bg-g1 bg-cover bg-center hover:bg-right duration-300 p-8 h-1/4 xl:h-full flex flex-col justify-center md:justify-between"
             @click="render('Team')">
                 <h1 class="text-5xl font-bold">Plantilla</h1>
                 <p class="text-xl hidden md:inline"></p>
             </div>
 
-            <div class="bg-g3 bg-cover bg-center p-8 h-1/4 xl:h-full flex flex-col justify-center md:justify-between"
+            <div class="bg-g3 bg-cover bg-center hover:bg-right duration-300 p-8 h-1/4 xl:h-full flex flex-col justify-center md:justify-between"
             @click="render('News')">
                 <h1 class="text-5xl font-bold">Noticias</h1>
                 <p class="text-xl hidden md:inline"></p>
